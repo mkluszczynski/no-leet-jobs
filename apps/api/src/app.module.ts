1f6fe21c4e524510bd90219c40f58609a0989755
mkluszczynski <mkluszczynski07@gmail.com>
2025-03-29 23:03:52 +0100

    Removed unused controller and service

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { toBoolean } from '../utils/boolean';
@@ -28,7 +26,5 @@ import { JobsModule } from './jobs/jobs.module';
     }),
     JobsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
