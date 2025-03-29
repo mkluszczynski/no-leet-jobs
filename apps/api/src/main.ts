a978de4bff284432246a067f88d23a36a354b0a2
mkluszczynski <mkluszczynski07@gmail.com>
2025-03-29 21:23:49 +0100

    Added swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api/v1');
+
   const configService = app.get(ConfigService);
 
-  const port = configService.get("APP_PORT");
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
 
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('/api/v1/swagger', app, documentFactory);
+
+  const port = configService.get('APP_PORT');
   await app.listen(port);
 }
 bootstrap();
