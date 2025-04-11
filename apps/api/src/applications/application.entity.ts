094e27489948afde4dd039084f947d3686026df0
mkluszczynski <mkluszczynski07@gmail.com>
2025-04-11 15:35:38 +0200

    Application entity

--- apps/api/src/applications/application.entity.ts
+++ apps/api/src/applications/application.entity.ts
@@ -1,4 +1,22 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class Application {}
+export class Application {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  createdAt: Date;
+}
