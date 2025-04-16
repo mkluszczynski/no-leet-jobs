1686c1a11d35a3ed615f3730f378ff0216d4680f
mkluszczynski <mkluszczynski07@gmail.com>
2025-04-16 10:53:06 +0200

    Added file validation

--- apps/api/src/applications/applications.controller.ts
+++ apps/api/src/applications/applications.controller.ts
@@ -1,4 +1,16 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  ParseFilePipeBuilder,
+  Post,
+  Put,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ApplicationsService } from './applications.service';
 import { IdParam } from 'src/utils/common/ByIdParam';
 import { ApplicationDto } from './dto/application.dto';
@@ -7,23 +19,19 @@ import { ApiConsumes } from '@nestjs/swagger';
 
 @Controller('applications')
 export class ApplicationsController {
+  constructor(private readonly applicationsService: ApplicationsService) {}
 
-    constructor(
-        private readonly applicationsService: ApplicationsService,
-    ) {}
+  @Get()
+  getAllApplications() {
+    return this.applicationsService.getAllApplications();
+  }
 
-    @Get()
-    getAllApplications() {
-        return this.applicationsService.getAllApplications();
-    }
+  @Get(':id')
+  getApplicationById(@Param() params: IdParam) {
+    return this.applicationsService.getApplicationById(params.id);
+  }
 
-    @Get(':id')
-    getApplicationById(@Param() params: IdParam) {
-        return this.applicationsService.getApplicationById(params.id);
-    }
-
-
-    /*
+  /*
     curl -X POST http://localhost:3001/api/v1/applications \
     -H "Content-Type: multipart/form-data" \
     -F "file=@/path/file.pdf" \
@@ -32,28 +40,35 @@ export class ApplicationsController {
     -F "email=test@example.com" \
     -F "phone=+48123456789"
     */
-    @Post()
-    @ApiConsumes('multipart/form-data')
-    @UseInterceptors(FileInterceptor('file'))
-    createApplication(
-        @Body() dto: ApplicationDto,
-        @UploadedFile() file: Express.Multer.File,
-    ) {
-        console.log("dto", dto);
-        console.log("file", file); 
-        return this.applicationsService.createApplicationFromDtoAndFile(dto, file);
-    }
+  @Post()
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('file'))
+  createApplication(
+    @Body() dto: ApplicationDto,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: 'application/pdf',
+        })
+        .addMaxSizeValidator({
+          maxSize: 4194304, // 4MB
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return this.applicationsService.createApplicationFromDtoAndFile(dto, file);
+  }
 
-    @Put(':id')
-    updateApplicationById(
-        @Param() params: IdParam,
-        @Body() dto: ApplicationDto,
-    ) {
-        return this.applicationsService.updateApplicationFromDto(params.id, dto);
-    }
+  @Put(':id')
+  updateApplicationById(@Param() params: IdParam, @Body() dto: ApplicationDto) {
+    return this.applicationsService.updateApplicationFromDto(params.id, dto);
+  }
 
-    @Delete(':id')
-    deleteApplicationById(@Param() params: IdParam) {
-        return this.applicationsService.deleteApplication(params.id);
-    }
+  @Delete(':id')
+  deleteApplicationById(@Param() params: IdParam) {
+    return this.applicationsService.deleteApplication(params.id);
+  }
 }
